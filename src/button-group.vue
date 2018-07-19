07dca8ff65cbaeed800216bc7da9fd09fa3f390e
yulilong <yulilong222q@outlook.com>
2018-07-20 00:33:20 +0800

    创建 button group

---
+++ src/button-group.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="g-button-group">
+  <slot></slot>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+  .g-button-group {
+    display: inline-flex;
+    // vertical-align: middle;
+    > .g-button {
+      border-radius: 0;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      // 解决三个按钮在一起边框在一起，只留下一个
+      margin-left: -1px;
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
